64bb33614b1a5efa6615b943a8f2c93565d0aa0b
Emre <e.hatay@vikes.csuohio.edu>
2016-02-11 16:38:05 +0000

    Improvements on motor_controller script

--- tara_firmware/arduino/tara_motor_controller/tara_motor_controller.ino
+++ tara_firmware/arduino/tara_motor_controller/tara_motor_controller.ino
@@ -1,68 +1,192 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Bool.h>
 
 ros::NodeHandle nh;
-const int r_motor_pin_f = 5;
-const int l_motor_pin_f = 10;
-const int r_motor_pin_b = 6;
-const int l_motor_pin_b = 11;
 
-void messageCbLb( const std_msgs::Int16& msg)
+/* Right motor */
+const int motor1_pwm = 11;
+const int motor1_en = 12;
+const int motor1_dir = 13;
+boolean motor1_stopped = true;
+boolean motor1_enabled = false;
+boolean motor1_forward = true;
+
+/* Left motor */
+const int motor2_pwm = 10;
+const int motor2_en = 8;
+const int motor2_dir = 9;
+boolean motor2_stopped = true;
+boolean motor2_enabled = false;
+boolean motor2_forward = true;
+
+const int EMF_wait_buffer_msecs = 50;
+
+void change_motor_direction (int motor, boolean forward)
 {
-  if(msg.data <= 255 || msg.data >= 0)
+  if(motor == 1)
   {
-    analogWrite(l_motor_pin_f, 0);
-    analogWrite(l_motor_pin_b, msg.data); 
+    motor1_forward = forward;
+    if(forward)
+    {
+      digitalWrite(motor1_dir, HIGH);
+      delay(EMF_wait_buffer_msecs);
+    }
+    else
+    {
+      digitalWrite(motor1_dir, LOW);
+      delay(EMF_wait_buffer_msecs);
+    }
+  }
+  else if(motor == 2)
+  {
+    motor2_forward = forward;
+    if(forward)
+    {
+      digitalWrite(motor2_dir, HIGH);
+      delay(EMF_wait_buffer_msecs);
+    }
+    else
+    {
+      digitalWrite(motor2_dir, LOW);
+      delay(EMF_wait_buffer_msecs);
+    }
   }
 }
-void messageCbLf( const std_msgs::Int16& msg)
+
+void slow_stop_motor(int motor)
 {
-  if(msg.data <= 255 || msg.data >= 0)
+  if(motor == 1)
   {
-    analogWrite(l_motor_pin_b, 0);
-    analogWrite(l_motor_pin_f, msg.data); 
+    motor1_enabled = false;
+    motor1_stopped = true; 
+    digitalWrite(motor1_en, LOW);
+  }
+  else if (motor == 2)
+  {
+    motor2_enabled = false;
+    motor2_stopped = true;
+    digitalWrite(motor2_en, LOW);
   }
 }
 
-void messageCbRf( const std_msgs::Int16& msg)
+void enable_motor(int motor)
 {
-  if(msg.data <= 255 || msg.data >= 0)
+  if(motor == 1)
   {
-    analogWrite(r_motor_pin_b, 0);
-    analogWrite(r_motor_pin_f, msg.data); 
-  } 
-}
-
-
-void messageCbRb( const std_msgs::Int16& msg)
-{
-  if(msg.data <= 255 || msg.data >= 0)
+    digitalWrite(motor1_en, HIGH);
+    motor1_enabled = true;
+  }
+  else if (motor == 2)
   {
-    analogWrite(r_motor_pin_f, 0);
-    analogWrite(r_motor_pin_b, msg.data); 
+    digitalWrite(motor2_en, HIGH);
+    motor2_enabled = true;
   }
 }
 
-ros::Subscriber<std_msgs::Int16> sublf("hardware/lmotor/forward", &messageCbLf );
-ros::Subscriber<std_msgs::Int16> sublb("hardware/lmotor/backward", &messageCbLb );
-ros::Subscriber<std_msgs::Int16> subrf("hardware/rmotor/forward", &messageCbRf );
-ros::Subscriber<std_msgs::Int16> subrb("hardware/rmotor/backward", &messageCbRb );
+void quick_stop_motor(int motor)
+{
+  if(motor == 1)
+  {
+    motor1_stopped = true;
+  }
+  else if (motor == 2)
+  {
+    motor2_stopped = true;
+  }
+}
 
+void motor1_signal( const std_msgs::Int16& msg)
+{
+  int cmd = msg.data;
+  if(msg.data < 0)
+  {
+    if(motor1_forward)
+      change_motor_direction(1,false);
+  }
+  else if (msg.data > 0)
+  {
+    if(!motor1_forward)
+      change_motor_direction(1,true);
+  }
+  else if (msg.data == 0)
+  {
+    quick_stop_motor(1);
+  }
+  analogWrite(motor1_pwm, abs(msg.data));
+}
+
+void motor2_signal( const std_msgs::Int16& msg)
+{
+  if(msg.data < 0)
+  {
+    if(motor2_forward)
+      change_motor_direction(2,false);
+  }
+  else if (msg.data > 0)
+  {
+    if(motor2_forward)
+      change_motor_direction(2,true);
+  }
+  else if (msg.data == 0)
+  {
+    if(motor2_stopped == false)
+      quick_stop_motor(2);
+  }
+  analogWrite(motor2_pwm, abs(msg.data));
+}
+
+void motor1_state( const std_msgs::Bool& msg)
+{
+  if(msg.data)
+  {
+    enable_motor(1);
+  }
+  else
+  {
+    slow_stop_motor(1); 
+  }
+}
+
+void motor2_state( const std_msgs::Bool& msg)
+{
+  if(msg.data)
+  {
+    enable_motor(2);
+  }
+  else
+  {
+    slow_stop_motor(2); 
+  }
+}
+ros::Subscriber<std_msgs::Int16> sub_motor2_signal("tara_firmware/motor2/signal", &motor2_signal );
+ros::Subscriber<std_msgs::Int16> sub_motor1_signal("tara_firmware/motor1/signal", &motor1_signal );
+
+ros::Subscriber<std_msgs::Bool> sub_motor2_state("tara_firmware/motor2/change_state", &motor2_state );
+ros::Subscriber<std_msgs::Bool> sub_motor1_state("tara_firmware/motor1/change_state", &motor1_state );
 void setup()
 {
+  pinMode(motor1_pwm, OUTPUT);
+  pinMode(motor2_pwm, OUTPUT);
+  pinMode(motor1_en, OUTPUT);
+  pinMode(motor2_en, OUTPUT);
+  pinMode(motor1_dir, OUTPUT);
+  pinMode(motor2_dir, OUTPUT);
+  
+  digitalWrite(motor1_en, LOW);
+  digitalWrite(motor2_en, LOW);
+  digitalWrite(motor1_dir, HIGH);
+  digitalWrite(motor2_dir, HIGH);
+
   nh.initNode();
-  nh.subscribe(sublf);
-  nh.subscribe(sublb);
-  nh.subscribe(subrb);
-  nh.subscribe(subrf);
+  nh.subscribe(sub_motor1_signal);
+  nh.subscribe(sub_motor2_signal);
+  nh.subscribe(sub_motor1_state);
+  nh.subscribe(sub_motor2_state);
 }
 
 void loop()
 {
-  pinMode(r_motor_pin_b,OUTPUT);
-  pinMode(l_motor_pin_b,OUTPUT);
-  pinMode(r_motor_pin_f,OUTPUT);
-  pinMode(l_motor_pin_f,OUTPUT);
   nh.spinOnce();
   delay(1);
 }

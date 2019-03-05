1d59af5d17dbe195aab3891722046e1c2a6be0fe
Emre Hatay <e.hatay@vikes.csuohio.edu>
2019-03-05 13:34:18 -0500

    Added support for encoder readings

---
+++ tara_2dnav/scripts/system_setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from system import Encoder
+import rospy,sys,os
+
+name = sys.argv[1]
+enc1_pins = rospy.get_param("/"+name+"/pins")
+enc1_device_name = rospy.get_param("/"+name+"/device_name")
+enc1_printToScreen = rospy.get_param("/"+name+'/print_to_screen', False)
+enc1_topic = rospy.get_param("/"+name+"/topic")
+
+e1 = Encoder(enc1_pins['A'], enc1_pins['B'], enc1_pins['S'], enc1_device_name, enc1_printToScreen, enc1_topic)
+e1.RosLoop()
